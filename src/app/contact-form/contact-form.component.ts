13dfe7d8329e6b3acfed20d4bb987958a48c7faf
Okiemute Gold <goldokiemute1@gmail.com>
2022-04-23 15:02:36 +0100

    button disabled, check boxes, drop-down lists, and radio buttons

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -5,8 +5,30 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
+
 export class ContactFormComponent implements OnInit {
   comment: string = "";
+  contactMethods = [
+    {
+      id: 1,
+      name: 'Email'
+    },
+    {
+      id: 2,
+      name: 'Phone'
+    },
+  ];
+
+  contactMethodsEx2 = [
+    {
+      id: 1,
+      name: 'Email'
+    },
+    {
+      id: 2,
+      name: 'Phone'
+    },
+  ];
 
   log(firstName: any) {
     console.log(firstName);
